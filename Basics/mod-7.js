3e2664eb8ac6cb36f01a6f2a41da65248086cc4f
Amit Gangwar <gangwaramit449@gmail.com>
2024-07-05 09:18:24 +0530

    Conversion of Data Types

---
+++ Basics/mod-7.js
@@ -0,0 +1,22 @@
+const score = "5365abc";
+console.log(typeof score);
+
+const valNumber = Number(score);
+console.log(typeof valNumber);
+console.log(valNumber);
+
+const isLog = 1;
+console.log(isLog);
+console.log(typeof isLog);
+
+const booleanLog = Boolean(isLog);
+console.log(typeof booleanLog);
+console.log(booleanLog);
+
+/*
+ Boolean 
+ * 1 -> true 
+ * 0 -> false 
+ * "Raju" -> true 
+ * "" -> false 
+*/
