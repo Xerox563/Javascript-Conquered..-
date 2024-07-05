58a9fa4bff471c36057a9101f2ffae1c262ee0d5
Amit Gangwar <gangwaramit449@gmail.com>
2024-07-05 08:59:09 +0530

    Learnt about null and undefined datatypes

---
+++ Basics/mod-6.js
@@ -0,0 +1,22 @@
+"use Strict"; // Use all JS code as newer version of code
+
+//alert("Hello Amit !!"); // Not works here it works on console.
+// We are using Nodejs not browser
+
+/*
+ number => 2^53
+ bigint
+ string => ""
+ boolean => True/ False
+ null => standalone value  (Khaaali => empty)
+ undefined => declaered but value not given.
+ symbol=> unique
+ object
+ */
+
+const age = 34;
+console.log(typeof age);  // number
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
+
+
