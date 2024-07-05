122d3861a74310809cab36a3be54f4476e85d47c
Amit Gangwar <gangwaramit449@gmail.com>
2024-07-05 09:42:04 +0530

    Operations

---
+++ Basics/mod-8.js
@@ -0,0 +1,15 @@
+let str1 = "Hello";
+let str2 = "World";
+
+console.log(str1 + str2);
+
+const s1 = "1" + 2;
+const s2 = 1 + "2";
+const s3 = "1" + 2 + 3; // stringify all
+const s4 = 7 + 2 + "3" + 4 + 5; // Add then stringify
+
+console.table([s1, s2, s3, s4]);
+
+/*
+ *  Learn About Postfix and infix Operators
+ */
